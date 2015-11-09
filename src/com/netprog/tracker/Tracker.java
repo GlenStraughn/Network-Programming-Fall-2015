1de4062659d4eb0b7fb75c467d71c7afd25f473d
Tao <taokarlchen@gmail.com>
2015-11-09 11:33:18 -0600

    loadingProperties

--- src/com/netprog/tracker/Tracker.java
+++ src/com/netprog/tracker/Tracker.java
@@ -1,5 +1,8 @@
 package com.netprog.tracker;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -22,7 +25,7 @@ public class Tracker {
 
 	private static Map<String, NodeMetaData> nodesMetaDataMap = new HashMap<>();
 
-	private static Properties settings;
+	private static Properties settings = new Properties();
 
 	public Tracker() {
 		loadSettings();
@@ -30,12 +33,33 @@ public class Tracker {
 
 	private void loadSettings() {
 		// TODO Auto-generated method stub
-
+		try {
+			FileInputStream fis = new FileInputStream("settings.props");
+			settings.load(fis);
+			System.out.println(settings.getProperty("test"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 	protected List<Node> sendPeerList() {
 		List<Node> peerList = new ArrayList<>();
 		return peerList;
+	}
+
+	
+	
+	public static Properties getSettings() {
+		return settings;
+	}
+
+	public static void setSettings(Properties settings) {
+		Tracker.settings = settings;
 	}
 
 }
