765e48f3be9dd3a6b7104c6e65b4151ce462cb6e
GlenStraughn <glenstraughn@juno.com>
2015-11-09 12:07:41 -0600

    Very Basic Shell Code
    Pushed a very basic shell-code for the project's main method file.

---
+++ src/com/netprog/BitTorrentSimulator.java
@@ -0,0 +1,25 @@
+package com.netprog;
+//=======================================================//
+// BitTorrentSimulator
+// 	~ Main class for free rider detection algorithm
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Written for Computer Network Programming
+// Dr. Eric Chan Tin
+//-------------------------------------------------------//
+// DATE: 11.09.2015
+// IDE: Eclipse (Java)
+//=======================================================//
+
+import java.io.File;
+import java.util.*;
+
+public class BitTorrentSimulator
+{
+	private File settingsFile;
+	private int duration;
+	
+	public static void main(String args[])
+	{
+		
+	}
+}
