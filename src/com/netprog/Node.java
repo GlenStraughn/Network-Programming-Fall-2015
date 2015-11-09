00ca1852ecefc6a4d1284562582b9b42ea245ac7
VickyCuiweiqi <weiqicui@weiqis-mbp.osustudent.okstate.edu>
2015-11-09 12:13:05 -0600

    new

--- src/com/netprog/Node.java
+++ src/com/netprog/Node.java
@@ -1,5 +1,30 @@
 package com.netprog;
 
 public class Node {
-
+	public int NodeID;//Node identifier
+	private SimulationFile[] SFile=new SimulationFile[100];//list of files
+	public SimulationFile[] PFile=new SimulationFile[100];//list of published files
+	private float PublicPro=0;
+	private float DownloadPro=0;
+	
+	//Initialize Node
+	Node(int NodeID){	
+		this.NodeID=NodeID;
+	}
+	Node(int NodeID, float PublicPro, float DownloadPro){
+		this.NodeID=NodeID;
+		this.PublicPro=PublicPro;
+		this.DownloadPro=DownloadPro;
+	}
+	public float getPublicPro(){
+		return this.PublicPro;
+	}
+	public float getDownloadPro(){
+		return this.DownloadPro;
+	}
+	
+	//Add simulation files in Node, upper bound 100;
+	public void AddFile(SimulationFile SF, int index){
+		this.SFile[index]=SF;
+	}
 }
---
+++ src/com/netprog/SimulationFile.java
@@ -0,0 +1,16 @@
+package com.netprog;
+
+public class SimulationFile {
+
+	public int UID;//file identifier
+	public float size;//unit Mb
+	public int polularity;//range from 1 to 10
+	public int LoadAmount;//Amount Loaded
+	SimulationFile(int UID, float size, int popularity, int LoadAmount){
+		this.UID=UID;
+		this.size=size;
+		this.polularity=popularity;
+		this.LoadAmount=LoadAmount;
+		
+	}
+}
