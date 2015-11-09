55a60fe44fc9ae261f345e4aad18cc1c4b9fd246
Tao <taokarlchen@gmail.com>
2015-11-09 11:48:26 -0600

    added comments

--- src/com/netprog/tracker/Tracker.java
+++ src/com/netprog/tracker/Tracker.java
@@ -12,7 +12,7 @@ import java.util.Properties;
 import com.netprog.Node;
 
 /**
- * 
+ * Tracker.java: keep tracking status of each node
  */
 
 /**
@@ -21,22 +21,27 @@ import com.netprog.Node;
  */
 public class Tracker {
 
+	//list of all nodes
 	private static List<Node> nodesList = new ArrayList<>();
 
+	//a map between metadata and nodes
 	private static Map<String, NodeMetaData> nodesMetaDataMap = new HashMap<>();
 
+	//settings from the file settings.props. Can be invoked within any classes
 	private static Properties settings = new Properties();
+	
+	//file path of the settings file
+	private String settingsPath = "settings.props";
 
 	public Tracker() {
-		loadSettings();
+		loadSettings();//loading settings file when initialize the Tracker
 	}
 
 	private void loadSettings() {
-		// TODO Auto-generated method stub
 		try {
-			FileInputStream fis = new FileInputStream("settings.props");
+			FileInputStream fis = new FileInputStream(settingsPath);
 			settings.load(fis);
-			System.out.println(settings.getProperty("test"));
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -47,17 +52,29 @@ public class Tracker {
 
 	}
 
+	/**
+	 * 
+	 * @return a list of peers
+	 */
 	protected List<Node> sendPeerList() {
 		List<Node> peerList = new ArrayList<>();
+		
 		return peerList;
 	}
 
+	/**
+	 * @return the settings
+	 */
 	public static Properties getSettings() {
 		return settings;
 	}
 
+	/**
+	 * @param settings the settings to set
+	 */
 	public static void setSettings(Properties settings) {
 		Tracker.settings = settings;
 	}
+
 
 }
