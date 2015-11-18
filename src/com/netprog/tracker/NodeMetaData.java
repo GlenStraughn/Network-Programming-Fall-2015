640660f841d99960c4f3795554c768e3951639d8
Tao <taokarlchen@gmail.com>
2015-11-18 12:14:04 -0600

    Tracker part code

--- src/com/netprog/tracker/NodeMetaData.java
+++ src/com/netprog/tracker/NodeMetaData.java
@@ -2,10 +2,12 @@ package com.netprog.tracker;
 
 public class NodeMetaData {
 	private double score = 0.0;
-	private String nodeName = "";
+	private Integer nodeID;
+	private boolean freeRiderFlag = false;
+	
 
-	public NodeMetaData(String nodeName, double score) {
-		this.nodeName = nodeName;
+	public NodeMetaData(Integer nodeID, double score) {
+		this.nodeID = nodeID;
 		this.score = score;
 	}
 
@@ -19,5 +21,24 @@ public class NodeMetaData {
 	public void setScore(double score) {
 		this.score = score;
 	}
+	
+	public void increaseScore(double increase) {
+		this.score = score + increase;
+	}
+	
+	public void decreaseScore(double decrease) {
+		this.score = score - decrease;
+	}
 
+	public void reCalculateFreeRider(){
+		if (this.score >= 0) {
+			freeRiderFlag = false;
+		}else{
+			freeRiderFlag = true;
+		}
+	}
+	
+	public boolean isFreeRider(){
+		return freeRiderFlag;
+	}
 }
--- src/com/netprog/tracker/Tracker.java
+++ src/com/netprog/tracker/Tracker.java
@@ -6,9 +6,11 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 import com.netprog.Node;
 import com.netprog.SimulationFile;
@@ -23,32 +25,50 @@ import com.netprog.SimulationFile;
  */
 public class Tracker {
 
-	//list of all nodes
-	private List<Node> nodesList = new ArrayList<>();
+	// list of all nodes
+	private static List<Node> nodesList = new ArrayList<>();
 
-	//a map between metadata and nodes
-	private Map<String, NodeMetaData> nodesMetaDataMap = new HashMap<>();
-	
-	// Map for files and a map for listing nodes that hold said files
-	private Map<Integer, SimulationFile> fileMap;
-	private Map<Integer, ArrayList<Node>> nodesWithFile; // Nodes in the list associated with the file UID have that
-	
+	// a map between metadata and nodes id
+	private static Map<Integer, NodeMetaData> nodesMetaDataMap = new HashMap<>();
 
-	//settings from the file settings.props. Can be invoked within any classes
+	// Map for files and a map for listing nodes (id) that hold said files (UID of file)
+	private static Map<Integer, Integer> filesMap;
+	// private Map<Integer, ArrayList<Node>> nodesWithFile; // Nodes in the list
+	// associated with the file UID have that
+
+	// settings from the file settings.props. Can be invoked within any classes
 	private static Properties settings = new Properties();
-	
-	//file path of the settings file
+
+	// file path of the settings file
 	private String settingsPath = "settings.props";
 
+	// record number of nodes
+	public static int numberOfNodes = 3;
+	// initial probility for nodes publish a file
+	public static double initPubProb = 0.4;
+	// initial probility for nodes download a file
+	public static double initDwldProb = 0.4;
+	// initial score for each nodes
+	public static int initScore = 1;
+	//the score to be decreased each time
+	public static int scoreDecrease = 1;
+	//the score to be increased each time
+	public static int scoreIncrease = 2;
+
 	public Tracker() {
-		loadSettings("DEFAULT");//loading settings file when initialize the Tracker
+		loadSettings();// loading settings file when initialize the Tracker
+		numberOfNodes = Integer.parseInt(settings.getProperty("NumberOfNodes"));
+		initPubProb = Double.parseDouble(settings.getProperty("InitPubProb"));
+		initDwldProb = Double.parseDouble(settings.getProperty("InitDwldProb"));
+		initScore = Integer.parseInt(settings.getProperty("InitScore"));
+		scoreDecrease = Integer.parseInt(settings.getProperty("ScoreDecrease"));
+		scoreIncrease = Integer.parseInt(settings.getProperty("ScoreIncrease"));
 	}
 
-	private void loadSettings(String settings) {
+	private void loadSettings() {
 		try {
-			FileInputStream fis = new FileInputStream(settingsPath);
-			settings.load(fis);
-			
+			settings.load(getClass().getResourceAsStream(settingsPath));
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -64,9 +84,7 @@ public class Tracker {
 	 * @return a list of peers
 	 */
 	protected List<Node> sendPeerList() {
-		List<Node> peerList = new ArrayList<>();
-		
-		return peerList;
+		return nodesList;
 	}
 
 	/**
@@ -77,14 +95,45 @@ public class Tracker {
 	}
 
 	/**
-	 * @param settings the settings to set
+	 * @param settings
+	 *            the settings to set
 	 */
 	public static void setSettings(Properties settings) {
 		Tracker.settings = settings;
 	}
 
-	void addFile(SimulationFile file)
-	{
-		//fileMap.
+	public static void publishFile(Node node, SimulationFile smFile) {
+		if (!nodesList.contains(node)) {
+			addNode(node);
+		}
+		filesMap.put(node.NodeID, smFile.UID);
+		nodesMetaDataMap.get(node.NodeID).increaseScore(scoreIncrease);
+	}
+	
+	public static List<Integer> queryAndDwldFile(Node node, Integer fileUID) {
+		List<Integer> resourceNodesList = new ArrayList<>();
+		if (!nodesList.contains(node)) {
+			addNode(node);
+		}
+		if (filesMap.containsValue(fileUID)) {
+			Iterator<Integer> keysIterator = filesMap.keySet().iterator();
+			while(keysIterator.hasNext()){
+				Integer key = keysIterator.next();
+				if (fileUID == filesMap.get(key)) {
+					resourceNodesList.add(key);
+				}
+			}
+		}
+		if (resourceNodesList.size() > 0) {
+			nodesMetaDataMap.get(node.NodeID).decreaseScore(scoreDecrease);
+		}
+		
+		return resourceNodesList;
+	}
+
+	public static void addNode(Node node) {
+		nodesList.add(node);
+		NodeMetaData nodeMetaData = new NodeMetaData(node.NodeID, initScore);
+		nodesMetaDataMap.put(node.NodeID, nodeMetaData);
 	}
 }
